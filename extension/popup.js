200ed145086fbe1081c7afbc5526891176ab70da
Doron Sinai <doron@ip-10-0-0-9.ec2.internal>
2014-09-30 17:05:28 +0300

    f

--- extension/popup.js
+++ extension/popup.js
@@ -1,20 +1,16 @@
 // by passing an object you can define default values e.g.: []
-/*chrome.storage.sync.get({videos: {}}, function (result) {
-    // the input argument is ALWAYS an object containing the queried keys
-    // so we select the key we need
-    var videos = result.videos;
-    videos["videoId1"] = {
-      videoTitle:"title",
-      videoDuration:440,
-      videoSavedTime:300
+var data = {};
+  chrome.storage.sync.get({videos: {}}, function (result) {
+    data = result;
+    for (key in data.videos){
+      $('body').after('<div><img src="' + data.videos[key].thumbnail + '"/><span>'+ data.videos[key].title +'</span></div>');
+    }
+  });
+
+
+$( document ).ready(function() {
+    for (key in data.videos){
+      $('body').after('<div><img src="' + data.videos[key].thumbnail + '"/><span>'+ data.videos[key].title +'</span></div>');
     }
 
-    // set the new array value to the same key
-    chrome.storage.local.set({videos: videos}, function () {
-        // you can use strings instead of objects
-        // if you don't  want to define default values
-        chrome.storage.local.get('videos', function (result) {
-            console.log(result.videos)
-        });
-    });
-});*/
+});
